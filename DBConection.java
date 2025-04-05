65f935586b79629d62861098e499f0505c0719c8
vijaysinghfartyal <vijaysinghfartyal0@gmail.com>
2025-04-05 18:39:12 +0530

    Add files via upload

---
+++ DBConection.java
@@ -0,0 +1,29 @@
+package dbconn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConection {
+    private static Connection conn = null;
+    public static Connection  getConnection() {
+		 System.out.println("GetConnection ==== ");
+		 try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","vijay7248");
+			System.out.println("Conn success "+conn);
+			
+		} catch (Exception e) {
+           e.printStackTrace();
+		}
+		 
+    	
+    	return conn;
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println(DBConection.getConnection());
+
+	}
+
+}
